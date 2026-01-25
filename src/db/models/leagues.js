905859a4ed24f46cd4a8eba3704b890b4555bc6f
Sergey Shulga <shul.sergio@gmail.com>
2026-01-25 23:52:02 +0200

    add league schema

---
+++ src/db/models/leagues.js
@@ -0,0 +1,27 @@
+import { model, Schema } from 'mongoose';
+
+const LeagueSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+      required: true,
+    },
+    avatarUrl: {
+      type: String,
+      // default: 'league-avatar.png',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const LeagueCollection = model('leagues', LeagueSchema);
