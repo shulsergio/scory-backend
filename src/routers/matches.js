92b453b50562cb9cdf06b17b4bf589be599d22ce
Sergey Shulga <shul.sergio@gmail.com>
2026-01-26 03:55:19 +0200

    Update matches.js

--- src/routers/matches.js
+++ src/routers/matches.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import ctrlWrapper from '../utils/ctrlWrapper.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { getAllMatchesController } from '../controllers/matches.js';
 
 const matchesRouter = Router();
