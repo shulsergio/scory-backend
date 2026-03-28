ac8f74c06b78f869c905cfd6a052815817163fff
Sergey Shulga <shul.sergio@gmail.com>
2026-03-28 14:48:56 +0200

    fixx last err in matchController

--- src/controllers/matchController.js
+++ src/controllers/matchController.js
@@ -13,26 +13,11 @@ export const finishAndCalculateMatch = async (req, res) => {
     return res.status(403).json({ error: 'Неверный ключ.' });
   }
   try {
-    // 1. Пробуем найти вообще ВСЕ прогнозы этого матча (игнорируем флаги и типы)
-    const findAsString = await PredictorsCollection.find({
-      matchId: matchId,
-    }).lean();
-    const findAsObj = await PredictorsCollection.find({
-      matchId: new ObjectId(matchId),
-    }).lean();
-
-    console.log('--- Поиск строкой ---', findAsString.length);
-    console.log('--- Поиск через ObjectId ---', findAsObj.length);
-
-    // Если первый нашел, а второй нет — значит в базе СТРОКИ.
-    // Если оба по 0 — значит поле в базе называется по-другому или ID не тот.
-
     const predictions = await PredictorsCollection.find({
       $or: [{ matchId: matchId }, { matchId: new ObjectId(matchId) }],
-      isCalculated: { $ne: true }, // Ищем всё, что не рассчитано
+      isCalculated: { $ne: true },
     }).lean();
 
-    // Теперь predictions — это обычный массив [{}, {}, ...]
     console.log('---Найдено прогнозов---', predictions.length);
     console.log('---matchId---', matchId);
     console.log('---predictions---', predictions);
@@ -67,7 +52,7 @@ export const finishAndCalculateMatch = async (req, res) => {
       userUpdates.push({
         updateOne: {
           filter: { _id: pred.userId },
-          update: { $inc: { totalPoints: points } },
+          update: { $inc: { points: points } },
         },
       });
     });
