ea34699410135c20a66c0c1af375e5a7b86fc477
Sergey Shulga <shul.sergio@gmail.com>
2026-06-29 03:36:23 +0300

    Update matches.js

--- src/service/matches.js
+++ src/service/matches.js
@@ -1,4 +1,5 @@
 import { MatchesCollection } from '../db/models/matches.js';
+import { matchPreviewsCollection } from '../db/models/matchPreviews.js';
 
 export const getMatchByIdData = async (matchId) => {
   const match = await MatchesCollection.findById(matchId)
@@ -6,6 +7,25 @@ export const getMatchByIdData = async (matchId) => {
     .populate('awayTeam')
     .populate('tournament', 'name')
     .lean();
+
+  if (!match) return null;
+
+  match.preview = null;
+
+  if (match.status === 'scheduled' && match.fotmobId) {
+    const previewData = await matchPreviewsCollection
+      .findOne({ fotmobId: match.fotmobId })
+      .lean();
+
+    if (previewData) {
+      match.preview = previewData;
+    }
+  }
+
+  //   Если матч закончился, то думать тут
+  // if (match.status === 'finished') {
+  //   match.stats = await matchStatsCollection.findOne({ ... });
+  // }
 
   return match;
 };
