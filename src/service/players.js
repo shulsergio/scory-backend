333c65d222a8fcca393a11730d2b1716fbfa869d
Sergey Shulga <shul.sergio@gmail.com>
2026-09-18 01:13:44 +0300

    fix err service

--- src/service/players.js
+++ src/service/players.js
@@ -1,17 +1,25 @@
 import createHttpError from 'http-errors';
 import { PlayersCollection } from '../db/models/players.js';
+import mongoose from 'mongoose';
 
 export const getPlayerByIdService = async (id) => {
-  // Ищем либо по MongoDB _id, либо по fotmobId
-  const player = await PlayersCollection.findOne({
-    $or: [{ _id: id }, { fotmobId: Number(id) || 0 }],
-  })
-    .populate('team.id', 'name logoUrl')
-    .lean();
+  const isMongoId = mongoose.Types.ObjectId.isValid(id);
+  const numericId = Number(id);
+
+  const filter = isMongoId
+    ? { _id: id }
+    : { fotmobId: !isNaN(numericId) ? numericId : 0 };
+
+  const player = await PlayersCollection.findOne(filter).lean();
 
   if (!player) {
     throw createHttpError(404, 'Player not found');
   }
 
-  return player;
+  if (player.team?.id && mongoose.Types.ObjectId.isValid(player.team.id)) {
+    await PlayersCollection.populate(player, {
+      path: 'team.id',
+      select: 'name logoUrl',
+    });
+  }
 };
