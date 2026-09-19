061bf1e015e4ea3e644a08f9df73dfcb32b9706d
Sergey Shulga <shul.sergio@gmail.com>
2026-09-19 17:09:01 +0300

    Update players.js

--- src/service/players.js
+++ src/service/players.js
@@ -9,17 +9,10 @@ export const getPlayerByIdService = async (id) => {
   const filter = isMongoId
     ? { _id: id }
     : { fotmobId: !isNaN(numericId) ? numericId : 0 };
-
   const player = await PlayersCollection.findOne(filter).lean();
 
   if (!player) {
     throw createHttpError(404, 'Player not found');
   }
-
-  if (player.team?.id && mongoose.Types.ObjectId.isValid(player.team.id)) {
-    await PlayersCollection.populate(player, {
-      path: 'team.id',
-      select: 'name logoUrl',
-    });
-  }
+  return player;
 };
